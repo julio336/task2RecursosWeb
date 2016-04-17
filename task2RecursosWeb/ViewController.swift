d197b54441ce0c047dec7432b42ec4028a770aaa
julio <julio336@hotmail.com>
2016-04-16 23:39:49 -0500

    image

--- task2RecursosWeb/ViewController.swift
+++ task2RecursosWeb/ViewController.swift
@@ -31,11 +31,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func isbnAction(sender: UITextField){
-        
-    }
-    
+ 
     func textFieldShouldReturn(textField: UITextField!) -> Bool {   //delegate method
         textField.resignFirstResponder()
         getData()
